22a5ad73d08d7f5780c3c7a59e95e19bb25f23ed
dpkdahiya <39999569+dpkdahiya@users.noreply.github.com>
2024-05-19 05:53:28 +0530

    added google and github oatuh

--- src/auth.js
+++ src/auth.js
@@ -2,6 +2,8 @@ import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaClient } from "@prisma/client";
 import crypto from "crypto";
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   providers: [
@@ -37,6 +39,15 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
 
         return user;
       },
+    }),
+
+    GoogleProvider({
+      clientId: process.env.GCLIENT_ID,
+      clientSecret: process.env.GCLIENT_SECRET,
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
 });
